0320569eac8e5c783798b29a62571d6c8445d37b
krenee411 <kelci.morgan@bryanuniversity.edu>
2022-09-20 16:56:08 -0500

    Update Python Fizzbuzz

--- week2/fizzbuzz/testKata.py
+++ week2/fizzbuzz/testKata.py
@@ -1,27 +1,27 @@
 # def testFizzBuzzFunction():
 #     fizzbuzz()
-# 
+#
 
 
 from PepsiCoke import fizzbuzz
 
 def test1():
-    fizzbuzz(1) == "1"
+    assert fizzbuzz(1) == "1"
 
 def test2():
-    fizzbuzz(2) == "2"
+    assert fizzbuzz(2) == "2"
 
 def test3():
-    fizzbuzz(3) == "Pepsi"
+    assert fizzbuzz(3) == "Pepsi"
 
 def test5():
-    fizzbuzz(5) == "Coke"
+    assert fizzbuzz(5) == "Coke"
 
 def test6():
-    fizzbuzz(6) == "Pepsi"
+    assert fizzbuzz(6) == "Pepsi"
 
 def test10():
-    fizzbuzz(10) == "Coke"
+    assert fizzbuzz(10) == "Coke"
 
 def test15():
     fizzbuzz(15) == "PepsiCoke"
