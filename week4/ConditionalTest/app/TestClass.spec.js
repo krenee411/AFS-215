13ff5707d998ca05a2459f6b6d988ec224e2ce21
krenee411 <kelci.morgan@bryanuniversity.edu>
2022-10-01 09:58:47 -0500

    Conditional Test added

--- week4/ConditionalTest/app/TestClass.spec.js
+++ week4/ConditionalTest/app/TestClass.spec.js
@@ -1,48 +1,57 @@
-const {expect, assert} = require('chai')
+const {expect} = require('chai')
 const ClassInst = require('./Class')
 
 
 describe('getting the proper outputs', () =>{
     it("gives a 1 string", () => {
         const CI = new ClassInst(1).timeOfDay();
+        expect(CI).to.equal("1")
         console.log("1: " + CI)
     })
     it("gives a 2 string", () => {
         const CI = new ClassInst(2).timeOfDay();
+        expect(CI).to.equal("2")
         console.log("2: " + CI)
         
     })
     it('says good morning when passed 3', () => {  
         const CI = new ClassInst(3).timeOfDay();
+        expect(CI).to.equal("Good Morning")
     console.log("3: " + CI)
        
     })
     it('says good afternoon when passed 7', () => {
         const CI = new ClassInst(7).timeOfDay();
+        expect(CI).to.equal("Good Afternoon")
     console.log("4: " + CI)
     })
     it('says good morning when passed multiple of 3', () => {  
         const CI = new ClassInst(6).timeOfDay();
+        expect(CI).to.equal("Good Morning")
     console.log("5: " + CI)
     })
     it('says good afternoon when passed multiple of 7', () => {
         const CI = new ClassInst(14).timeOfDay();
+        expect(CI).to.equal("Good Afternoon")
     console.log("6: " + CI)
     })
 
     it('says good evening when passed multiple of 3 and 7', () => {
         const CI = new ClassInst(21).timeOfDay();
+        expect(CI).to.equal("Good Evening")
         console.log("7: " + CI)
     })
 
     it('is neither a multiple of 3 or 7 returns string', () => {
         const CI = new ClassInst(19).timeOfDay();
+        expect(CI).to.be.a("string")
         console.log("8: " + CI)
     })
     
     it('throws an error', () => {
         const CI = new ClassInst("Hello World").timeOfDay();
-        console.log("7: " + error)
+        expect(CI).to.be.a("string")
+        console.log("7: " + CI)
     })
   
 })
