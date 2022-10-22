5b1891880e5c4d384876508a0ba1f165d26315c8
krenee411 <kelci.morgan@bryanuniversity.edu>
2022-10-22 13:34:43 -0500

    Capstone TodoList PyCharm Added

--- week7/Capstone/TestTodoList.py
+++ week7/Capstone/TestTodoList.py
@@ -1,8 +1,86 @@
+import pytest
 
-from todoList import MyClass
+from todoList import MyTodoList
 
 
-def testDummy():
-    item = MyClass()
-    assert item.add("hello") == "hello has been added"
+# def testCanGetList():
+#     item = MyTodoList()
+#     item.add('Hello')
+#     item.add(1)
+#     item.add({"Name": "Kelci"})
+#     item.add(True)
+#     assert item.getList() == ['Hello', 1, {"Name": "Kelci"}, True]
+#
+# def testCanAddToList():
+#     item = MyTodoList()
+#     assert item.add('Jump') == "Jump has been added"
+#
+# def testCanAddMultiItemToList():
+#     item = MyTodoList()
+#     item.add('Hello')
+#     item.add(1)
+#     item.add({"Name": "Kelci"})
+#     item.add(True)
+#     assert item.addMulti([False, 2, "Dragon"]) == ['Hello', 1, {"Name": "Kelci"}, True, False, 2, "Dragon"]
+#
+#
+# def testCanRemoveFirstFromList():
+#     item = MyTodoList()
+#     item.add('Hello')
+#     item.add(1)
+#     item.add({"Name": "Kelci"})
+#     item.add(True)
+#     assert item.removeFirst() == [1, {"Name": "Kelci"}, True]
+#
+#
+# def testCanRemoveLastFromList():
+#     item = MyTodoList()
+#     item.add('Hello')
+#     item.add(1)
+#     item.add({"Name": "Kelci"})
+#     item.add(True)
+#     assert item.removeLast() == ["Hello", 1, {"Name": "Kelci"}]
+#
+#
+# def testCanRemoveSpecificWordFromList():
+#     item = MyTodoList()
+#     item.add('Hello')
+#     item.add(1)
+#     item.add({"Name": "Kelci"})
+#     item.add(True)
+#     assert item.removeSpecific({"Name": "Kelci"}) == ["Hello", 1, True]
+#
+
+@pytest.fixture
+def item():
+    item = MyTodoList()
+    item.add('Hello')
+    item.add(1)
+    item.add({"Name": "Kelci"})
+    item.add(True)
+    return item
+
+
+def testCanGetList(item):
+    assert item.getList() == ['Hello', 1, {"Name": "Kelci"}, True]
+
+
+def testCanAddToList(item):
+    assert item.add('Jump') == "Jump has been added"
+
+
+def testCanAddMultiItemToList(item):
+    assert item.addMulti([False, 2, "Dragon"]) == ['Hello', 1, {"Name": "Kelci"}, True, False, 2, "Dragon"]
+
+
+def testCanRemoveFirstFromList(item):
+    assert item.removeFirst() == [1, {"Name": "Kelci"}, True]
+
+
+def testCanRemoveLastFromList(item):
+    assert item.removeLast() == ["Hello", 1, {"Name": "Kelci"}]
+
+
+def testCanRemoveSpecificWordFromList(item):
+    assert item.removeSpecific({"Name": "Kelci"}) == ["Hello", 1, True]
 
--- week7/Capstone/todoList.py
+++ week7/Capstone/todoList.py
@@ -1,6 +1,34 @@
-class MyClass:
-    def __int__(self):
+
+class MyTodoList:
+    def __init__(self):
         self.list = []
+        self.length = 0
+
+    def getList(self):
+        return self.list
 
     def add(self, word):
-        self.list.extend(word)
+        self.list.append(word)
+        self.length+1
+        return f'{word} has been added'
+
+    def addMulti(self, words):
+        self.list.extend(words)
+        self.length+1
+        return self.list
+
+    def removeFirst(self):
+        del self.list[0]
+        self.length-1
+        return self.list
+
+    def removeLast(self):
+        del self.list[-1]
+        self.length-1
+        return self.list
+
+    def removeSpecific(self, word):
+        found = self.list.index(word)
+        del self.list[found]
+        self.length-1
+        return self.list
